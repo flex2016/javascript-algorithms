6bbb601867eb5929b497471c7dd309059ff00316
flex2016 <reloded2004@hotmail.com>
2020-11-17 10:13:02 -0500

    added integer folder and moved files sumFibs

---
+++ integers/sumFibs.js
@@ -0,0 +1,38 @@
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+// The first two numbers in the Fibonacci sequence are 1 and 1.
+// Every additional number in the sequence is the sum of the two previous numbers.
+//  The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+// For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+
+// sumFibs(1) should return a number.
+// sumFibs(1000) should return 1785.
+// sumFibs(4000000) should return 4613732.
+// sumFibs(4) should return 5.
+// sumFibs(75024) should return 60696.
+// sumFibs(75025) should return 135721.
+
+function sumFibs(num) {
+  var prevNumber = 0;
+  var currNumber = 1;
+  var result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
+
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
+}
+
+// test here
+sumFibs(4);
+
+
+// Create a variable to keep record of the current and previous numbers along with the result that will be returned.
+// Use a while loop to make sure we do not go over the number given as parameter.
+// We use the modulo operand to check if the current number is odd or even. If it is odd, add it to the result.
+// Complete the Fibonacci circle by rotating getting the next number and swapping values after.
+// Return the result.
